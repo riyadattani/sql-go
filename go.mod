569aaca39aa2b65a4b1af0f6856a437a3c4d03df
Riya Dattani <riyaddattani@gmail.com>
2022-03-09 12:27:24 +0000

    use sqlxgit :D  way better

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module sql-go
 
 go 1.17
 
-require github.com/lib/pq v1.10.4 // indirect
+require (
+	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,50 +1,33 @@
 package main
 
 import (
-	"database/sql"
 	"log"
+	"time"
 
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
 type Movies struct {
-	id    int
-	title string
-	year  int
+	ID        int
+	Title     string
+	Year      int
+	CreatedAt time.Time `db:"created_at"`
 }
 
 func main() {
-	db, err := sql.Open("postgres", "postgres://postgres:password@localhost:5432/postgres?sslmode=disable")
+	db, err := sqlx.Open("postgres", "postgres://postgres:password@localhost:5432/postgres?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// insert a row
-	//result, err := db.Exec("insert into movies (title, year) VALUES ($1, $2)", "Lord of the rings 2", 2003)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//affected, _ := result.RowsAffected()
-	//log.Printf("Rows affected %d", affected)
-
-	// query for rows
-	rows, err := db.Query("select * from movies")
-	for rows.Next() {
-		movies := Movies{}
-		if err = rows.Scan(&movies.id, &movies.title, &movies.year); err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("movie is %v", movies)
-	}
-
-	if err := rows.Err(); err != nil {
+	movies := []Movies{}
+	err = db.Select(&movies, "select * from movies")
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	// query for a single row
-	var title string
-	row := db.QueryRow("select title from movies where id=1")
-	row.Scan(&title)
-	log.Printf("movie with id 1: %s", title)
+	for _, movie := range movies {
+		log.Printf("Movie is %v", movie.Title)
+	}
 }
